ac92316ddef5897161a7bedfb8dbf4705b6e087f
Nenekiri <mmiele.miele@gmail.com>
2018-08-30 23:40:29 -0400

    Added Memopad script that you can launch from the Windows tab

---
+++ Assets/Scripts/AddedTools.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AddedTools : MonoBehaviour {
+
+    [MenuItem("Tools/TestTool")]
+    private static void NewMenuOption()
+    {
+
+    }
+
+
+}//end of class
