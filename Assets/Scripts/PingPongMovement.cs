9de67e78258d08fe27050e7dabc2f3b46e44213b
Nenekiri <mmiele.miele@gmail.com>
2019-02-06 23:16:59 -0500

    Added eyepatch shark, logic for basic ping-pong movement, red X and part of treasure chest to correspond with shark

---
+++ Assets/Scripts/PingPongMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongMovement : MonoBehaviour {
+
+    public Vector3 pointB;
+    public float travelTime; 
+
+    IEnumerator Start()
+    {
+        var pointA = transform.position;
+        while (true)
+        {
+            yield return StartCoroutine(MoveObject(transform, pointA, pointB, travelTime));
+            yield return StartCoroutine(MoveObject(transform, pointB, pointA, travelTime));
+        }
+    }
+
+    IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+        var i = 0.0f;
+        var rate = 1.0f / time;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+            yield return null;
+        }
+    }
+}
