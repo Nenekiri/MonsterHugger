79d793ee0d68bc3e4c4053a0643d8227a24b1a26
Nenekiri <mmiele.miele@gmail.com>
2019-04-03 23:44:55 -0400

    Added Seaweed Octopus monster, tentacles that are huggable, prototyped Camera screenshot functionality

---
+++ Assets/Scripts/ScreenshotHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotHandler : MonoBehaviour {
+
+    public AudioSource audio;
+    public AudioClip cameraClick; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            string date = System.DateTime.Now.ToString();
+            date = date.Replace("/", "-");
+            date = date.Replace(" ", "_");
+            date = date.Replace(":", "-");
+            ScreenCapture.CaptureScreenshot(Application.dataPath + "/ScreenShots/SS_" + date + ".png");
+            audio.PlayOneShot(cameraClick);
+        }
+		
+	}
+
+}//end of class
---
+++ Assets/Scripts/SeaweedOctopus.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DoodleStudio95; 
+
+public class SeaweedOctopus : MonoBehaviour {
+
+    //define animation states to switch between
+    public DoodleAnimationFile animationIdle;
+    public DoodleAnimationFile animationAfterHug;
+
+    public float timer;
+    public DoodleAnimator animator;
+    public float hugTime; //normally set to 3.0f, which is 3 seconds but declaring this variable so I can go through and set different times for other creatures
+
+    public GameObject tentacle1;
+    public GameObject tentacle2;
+    public GameObject tentacle3;
+    public GameObject tentacle4;
+    public GameObject tentacle5;
+    public GameObject tentacle6;
+    public GameObject tentacle7;
+    public GameObject tentacle8;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        tentacle1.SetActive(false);
+        tentacle2.SetActive(false);
+        tentacle3.SetActive(false);
+        tentacle4.SetActive(false);
+        tentacle5.SetActive(false);
+        tentacle6.SetActive(false);
+        tentacle7.SetActive(false);
+        tentacle8.SetActive(false);
+    }//end of start method
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check the timer to see if we can change the animation
+        if (timer >= hugTime)
+        {
+            timer = 0.0f;
+            animator.ChangeAnimation(animationAfterHug);
+            tentacle1.SetActive(true);
+            tentacle2.SetActive(true);
+            tentacle3.SetActive(true);
+            tentacle4.SetActive(true);
+            tentacle5.SetActive(true);
+            tentacle6.SetActive(true);
+            tentacle7.SetActive(true);
+            tentacle8.SetActive(true);
+        }
+
+    }//end of update
+
+    //method to check for a collision and then allow for the hug input to be pressed
+    public void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //check to see if the player is currently hugging the object
+            if (Globals.isHugging == true)
+            {
+                timer += Time.deltaTime;
+            }
+        }
+    }//end of OnCollisionStay
+
+}//end of class
