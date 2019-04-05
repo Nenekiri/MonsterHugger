119b3c60785b7433a36c50c57993f477ee3dc63f
Nenekiri <mmiele.miele@gmail.com>
2019-04-05 00:12:12 -0400

    Added Camera stand and floating text above it, changed intro level floating text, and added base for wolf that acts as a tribute to Kyle

---
+++ Assets/Scripts/CameraHug.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHug : MonoBehaviour {
+
+
+    public float timer;
+    public float hugTime; //normally set to 3.0f, which is 3 seconds but declaring this variable so I can go through and set different times for other creatures
+
+    public GameObject floatingText; 
+
+    // Use this for initialization
+    void Start ()
+    {
+        floatingText.SetActive(false); 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (timer >= hugTime)
+        {
+            timer = 0.0f;
+            floatingText.SetActive(true); 
+        }
+    }
+
+    //method to check for a collision and then allow for the hug input to be pressed
+    public void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //check to see if the player is currently hugging the object
+            if (Globals.isHugging == true)
+            {
+                timer += Time.deltaTime;
+            }
+        }
+    }//end of OnCollisionStay
+
+
+}//end of class
